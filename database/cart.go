a0ccc4689758f1b0773882c703b9786c28e6fc41
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2024-02-08 00:37:18 -0500

    add databaseSetup code

---
+++ database/cart.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"errors"
+)
+
+var (
+	ErrCantFindProduct = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid = errors.New("this user is not valid")
+	ErrCantUpdateUser = errors.New("cannot add this product to the cart")
+	ErrCantRemoveItemFromCart = errors.New("canot remove this item from the cart")
+	ErrCantGetItem = errors.New("unable to get item from the cart")
+	ErrCantBuyCartItem = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
---
+++ database/databaseSetup.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/stripe/stripe-go/v76/client"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connect to MongoDB")
+
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
+}
