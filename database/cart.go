5fc9efc89bfcaf205b4f4327abd67ba1f76154a0
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2024-02-17 15:24:42 -0500

    add RemoveItemFromCart function

--- database/cart.go
+++ database/cart.go
@@ -52,8 +52,20 @@ func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo
 	return nil
 }
 
-func RemoveCartItem() {
-
+func RemoveCartItem(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.M{"$pull": bson.M{"usercart": bson.M{"_id": productID}}}
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+	if err != nil {
+		log.Println(err)
+		return ErrCantRemoveItemFromCart
+	}
+	return nil
 }
 
 func BuyItemFromCart() {
